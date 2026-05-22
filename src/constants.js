3a8b1553ea1ac7273b670cdc1813f4a4973312da
Tushar <khadketushar59@gmail.com>
2026-05-22 11:06:02 +0530

    intial commit

---
+++ src/constants.js
@@ -0,0 +1,300 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/kiranAcademy_logo.png';
+import agcLogo from './assets/company_logo/NareshIT_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/mit_logo.png';
+import bsaLogo from './assets/education_logo/12th_c_logo.png';
+import vpsLogo from './assets/education_logo/educational.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/electronics_store_03.png';
+import csprepLogo from './assets/work_logo/Travel.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+     // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      //{ name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      //{ name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+    //  { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Eclips', logo: mcLogo },
+     // { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+     // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Java Fullstack Course",
+      company: "Kiran Academy",
+      date: "Mar 2024 - Aug 2024",
+      desc: "Developed dynamic and scalable web applications using Java Full Stack technologies, including Java, Spring Boot, JDBC, Servlets, JSP, HTML, CSS, JavaScript, and MySQL. Built responsive user interfaces, implemented RESTful APIs, managed database operations, and followed MVC architecture to create efficient and user-friendly applications while working in an agile learning environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "java",
+        "Node JS",
+        "Tailwind CSS",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Spring Framework",
+      company: "Naresh it",
+      date: "Feb 2025 - Aug 2025",
+      desc: "Contributed to real-world projects as a Java Full Stack Developer trainee, working on both frontend and backend development using technologies such as Java, Spring Framework, Spring Boot, Hibernate, JDBC, HTML, CSS, JavaScript, Bootstrap, and MySQL. Collaborated with team members to build responsive and scalable web applications, implement backend business logic, integrate databases, and enhance application performance following industry-standard development practices.",
+      skills: [
+        "CoreJava",
+        "AdvanceJava",
+        "Spring",
+        "SpringBoot",
+        "SpringSecurity",
+        "DataJpa",
+        "Hibernate",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "Java Intern",
+      company: "ThunderCube ",
+      date: "Jun 2025 - Dec 2025",
+      desc: "Worked as a Java Developer Intern, developing responsive web applications using Java, JSP, Servlets, HTML, CSS, JavaScript, Bootstrap, and MySQL. Assisted in implementing backend functionalities, database integration, and user interface development while collaborating with the team to convert project requirements into interactive and user-friendly web applications.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Java",
+        "Sql",
+        "Material UI",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Solapur University",
+      date: "Jan 2020 - Mar 2024",
+      grade: "7.38 CGPA",
+      desc: "I have completed my Bachelor of Technology (B.Tech) in Computer Science and Engineering from Punyashlok Ahilyadevi Holkar Solapur University. During my academic journey, I built a strong foundation in programming, software development, and core computer science concepts. I studied subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, Software Engineering, and Java Technologies. I also participated in technical workshops and academic projects, which helped enhance my practical knowledge, problem-solving abilities, and overall professional development.",
+      degree: "B.tech in Computer Science and Engineering - CSE",
+    },
+    {
+      id: 1,
+      img: bsaLogo,
+      school: "SBZ College, Barshi",
+      date: "Sept 2019 - Aug 2020",
+      grade: "60%",
+      desc: "I completed my Higher Secondary Education (12th) in Science with PCM (Physics, Chemistry, and Mathematics). During my studies, I developed strong analytical and problem-solving skills along with a solid foundation in mathematics and science concepts. My academic background helped me build logical thinking abilities and sparked my interest in technology and computer science, which further motivated me to pursue a career in the software and IT industry.",
+      degree: "CBSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: vpsLogo,
+      school: "Sulakhe High School, Barshi",
+      date: "Apr 2017 - March 2018",
+      grade: "70%",
+      desc: "I completed my Secondary School Certificate (10th SSC) from Sulakhe High School, where I built a strong academic foundation in mathematics, science, and general studies. My schooling helped develop discipline, problem-solving abilities, and an interest in technology, which later motivated me to pursue Computer Science and Engineering.",
+      degree: "CBSE(X) - SSC",
+    },
+   /* {
+      id: 3,
+      img: vpsLogo,
+      school: "Vatsalya Public School Govardhan, Mathura",
+      date: "Apr 2015 - March 2016",
+      grade: "87.5%",
+      desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },*/
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Online Electronic Store",
+      description:
+        "A dynamic and user-friendly Online Electronic Store application developed to provide a seamless shopping experience for users. The platform allows customers to browse electronic products, view detailed product information, add items to the cart, and place orders efficiently. Designed with an intuitive interface and responsive design, the application ensures smooth navigation, secure user interaction, and effective product management for both customers and administrators.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Java","Oracle", "API"],
+      github: "https://github.com/tushar-khadake/Online-Electronic-Store",
+      //webapp: "https://githubprofiledetective.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "Travel and torism management system",
+      description:
+        "A console-based Travel and Tourism Management System developed using Java and JDBC to simplify travel booking and management operations. The application allows users to manage customer details, tour packages, hotel bookings, transportation records, and ticket reservations through an interactive menu-driven interface. Integrated with a relational database for efficient data storage and retrieval, the system provides a streamlined and user-friendly experience for handling travel and tourism operations.",
+      image: csprepLogo,
+      tags: ["Java Framework", "JDBC", "GUI", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/tushar-khadake/Travel-Management-System",
+      //webapp: "https://csprep.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Movie Recommendation App",
+      description:
+        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it a go-to app for movie enthusiasts.",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      //github: "https://github.com/codingmastr/Movie-Recommendation-App",
+     // webapp: "https://movie-recommendation-app-jet.vercel.app/",
+    },
+    /*{
+      id: 3,
+      title: "Persnal Portfilio",
+      description:
+        "A modern and responsive Personal Portfolio website developed to showcase projects, technical skills, certifications, and educational background in an interactive and professional manner. Built using HTML, CSS, JavaScript, Bootstrap, and React.js, the portfolio features smooth navigation, attractive UI design, project highlights, and contact functionality to create a strong online presence and improve professional visibility.",
+      image: npmLogo,
+      tags: ["React JS", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/tushar-khadake/tushar-khadake-portfolio",
+      //webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+    },*/
+    /*{
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Webverse Digital",
+      description:
+        "The official website for Webverse Digital, a creative digital marketing agency. Built using HTML, CSS, and JavaScript, it features visually appealing animations and a clean design to showcase the agency's services.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+      github: "https://github.com/codingmastr/Webverse-Digital",
+      webapp: "https://webversedigital.com/",
+    },
+    {
+      id: 6,
+      title: "Coding Master",
+      description:
+        "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+      github: "https://codingmasterweb.in/",
+      webapp: "https://codingmasterweb.in/",
+    },
+    {
+      id: 7,
+      title: "Image Search App",
+      description:
+        "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+      image: imagesearchLogo,
+      tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Search-App",
+      webapp: "https://imagsearch.netlify.app/",
+    },
+    {
+      id: 8,
+      title: "Image Background Remover",
+      description:
+        "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+      image: removebgLogo,
+      tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+      github: "https://github.com/codingmastr/Image-Background-Remover",
+      webapp: "https://removeyourbg.netlify.app/",
+    },*/
+  ];  
